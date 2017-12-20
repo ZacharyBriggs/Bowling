727be9b0fecd6fd520c62244d3c386fa313f5bd8
ZacharyBriggs <zacharybriggs64@gmail.com>
2017-12-20 12:48:00 -0600

    j

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Ball : MonoBehaviour
 {
+    public float timer = 3;
+
+    public Vector3 Direction;
 	// Use this for initialization
 	void Start ()
     {
-
+        Direction = new Vector3(1,0,0);
 	}
 	// Update is called once per frame
 	void Update ()
-    {
-
+	{
+	    if (timer > 0)
+	        timer -= Time.deltaTime;
+	    else
+	    {
+	        timer = 3;
+	        Direction = new Vector3(Random.Range(-1,1), Random.Range(-1,1), Random.Range(-1,1));
+            GetComponent<Renderer>().material.color = Random.ColorHSV();
+        }
+	    transform.position += Direction * Time.deltaTime;
 	}
 }
--- Assets/Round.cs
+++ Assets/Round.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Round : MonoBehaviour
 {
-    public List<Ball> balls;
+    public int mLocalScore;
+    public List<Ball> balls = new List<Ball>();
 	// Use this for initialization
 	void Start ()
-    {
-
+	{
+        mLocalScore = 0;
 	}
 	// Update is called once per frame
 	void Update ()
-    {
+	{
+	    if (Input.GetKey(KeyCode.Space))
+	    {
+	        GameObject newBall = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+	        newBall.AddComponent<Ball>();
+	        balls.Add(newBall.GetComponent<Ball>());
 
+        }
+	    if (Input.GetKeyDown(KeyCode.Mouse0))
+	    {
+	        mLocalScore++;
+	    }
 	}
 }
